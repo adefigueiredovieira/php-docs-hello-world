814f924f5d49bcbec39b56301c71a68eeb4dd612
Aslland-RRC <151769654+adefigueiredovieira@users.noreply.github.com>
2024-03-06 10:27:37 -0600

    First Change

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
         <footer class="bg-dark py-4 mt-auto">
             <div class="container px-5">
                 <div class="row align-items-center justify-content-between flex-column flex-sm-row">
-                    <div class="col-auto"><div class="small m-0 text-white">Daily Expense Trakcer  System (DETS) <?php echo date('Y');?></div></div>
+                    <div class="col-auto"><div class="small m-0 text-white">Daily Expense Tracker  System (DETS) <?php echo date('Y');?></div></div>
               
                 </div>
             </div>
